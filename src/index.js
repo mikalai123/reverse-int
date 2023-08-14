2a86c007307d4f8c87341abc77e346bcb00dc6f3
mikalai123 <mikalai.kavalionak@gmail.com>
2023-08-14 15:27:03 +0300

    fix:fix code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 module.exports = function reverse (n) {
     n = Math.abs(n);
     num = String(n).split("").reverse().join("");
-    // if(num[0] == " - ") {
-    //     num = num.shift();
-    //   }
     return num
 }
